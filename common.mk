2d66242821a16b5b868850a3cafdfe216114b324
Einar Gednochsson <pete80pro.pb@gmail.com>
2021-04-23 19:00:43 +0200

    sm8150-common: Drop Lineage Touchgestures

--- common.mk
+++ common.mk
@@ -403,10 +403,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.msmnile
-    
-    # Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.lenovo_msmnile
 
 # USB
 PRODUCT_PACKAGES += \
